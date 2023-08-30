ac544ae5915f4ae998a5631b3808020c870bcc02
wectro20 <shumskyy345@gmail.com>
2023-08-30 20:46:55 +0300

    migrate service tests to mockk

--- src/test/kotlin/com/ajax/cryptocurrency/service/CryptocurrencyServiceTest.kt
+++ src/test/kotlin/com/ajax/cryptocurrency/service/CryptocurrencyServiceTest.kt
@@ -13,6 +13,10 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
+import org.springframework.data.domain.*
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
@@ -55,114 +59,100 @@ class CryptocurrencyServiceTest {
         assertEquals(cryptocurrencyBTCPrice, result)
     }
 
-//    @Test
-//    fun findBTCMaxPriceTest() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("BTC", -1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("BTC", -1)
-//        assertEquals(cryptocurrencyBTCPrice, result)
-//    }
-//
-//    @Test
-//    fun findETHMinPriceTest() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("ETH", -1))
-//            .thenReturn(cryptocurrencyETHPrice)
-//        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("ETH", -1)
-//        assertEquals(cryptocurrencyETHPrice, result)
-//    }
-//
-//    @Test
-//    fun findETHMaxPriceTest() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("ETH", -1))
-//            .thenReturn(cryptocurrencyETHPrice)
-//        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("ETH", -1)
-//        assertEquals(cryptocurrencyETHPrice, result)
-//    }
-//
-//    @Test
-//    fun findXRPMinPriceTest() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("XRP", -1))
-//            .thenReturn(cryptocurrencyXRPPrice)
-//        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("XRP", -1)
-//        assertEquals(cryptocurrencyXRPPrice, result)
-//    }
-//
-//    @Test
-//    fun findXRPMaxPriceTest() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("XRP", -1))
-//            .thenReturn(cryptocurrencyXRPPrice)
-//        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("XRP", -1)
-//        assertEquals(cryptocurrencyXRPPrice, result)
-//    }
-//
-//    @Test
-//    fun getBTCPageTest() {
-//        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "BTC" }
-//            .sortedBy { it.price }
-//        val page = PageRequest.of(0, 10)
-//        val pageable = PageRequest.of(0, 10, Sort.by("price"))
-//        Mockito.`when`(cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("BTC", pageable))
-//            .thenReturn(toPage(sortedList, page))
-//        val result = cryptocurrencyService.getCryptocurrencyPages("BTC", 0, 10)
-//        assertEquals(sortedList, result)
-//    }
-//
-//    @Test
-//    fun getETHPageTest() {
-//        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "ETH" }
-//            .sortedBy { it.price }
-//        val page = PageRequest.of(0, 10)
-//        val pageable = PageRequest.of(0, 10, Sort.by("price"))
-//        Mockito.`when`(cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("ETH", pageable))
-//            .thenReturn(toPage(sortedList, page))
-//        val result = cryptocurrencyService.getCryptocurrencyPages("ETH", 0, 10)
-//        assertEquals(sortedList, result)
-//    }
-//
-//    @Test
-//    fun getXRPPageTest() {
-//        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "XRP" }
-//            .sortedBy { it.price }
-//        val page = PageRequest.of(0, 10)
-//        val pageable = PageRequest.of(0, 10, Sort.by("price"))
-//        Mockito.`when`(cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("XRP", pageable))
-//            .thenReturn(toPage(sortedList, page))
-//        val result = cryptocurrencyService.getCryptocurrencyPages("XRP", 0, 10)
-//        assertEquals(sortedList, result)
-//    }
-//
-//    @Test
-//    fun createCSV() {
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("BTC", -1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("BTC", 1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("ETH", -1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("ETH", 1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("XRP", -1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//        Mockito.`when`(cryptocurrencyRepository.findMinMaxByName("XRP", 1))
-//            .thenReturn(cryptocurrencyBTCPrice)
-//
-//        val result = cryptocurrencyService.writeCsv("cryptocurrency-prices-test")
-//        val actual = isEqual(Paths.get(expectedCsvFile), Paths.get(resultCsvFile))
-//        result.delete()
-//        assertEquals(true, actual)
-//    }
-//
-//    private fun toPage(list: List<Cryptocurrency>, pageable: Pageable): Page<Cryptocurrency> {
-//        val itemsInPage = list.drop(pageable.offset.toInt()).take(pageable.pageSize)
-//        return PageImpl(itemsInPage, pageable, list.size.toLong())
-//    }
-//
-//    private fun isEqual(firstFile: Path, secondFile: Path): Boolean {
-//        if (Files.size(firstFile) != Files.size(secondFile)) {
-//            return false
-//        }
-//        val first = Files.readAllBytes(firstFile)
-//        val second = Files.readAllBytes(secondFile)
-//        return first.contentEquals(second)
-//    }
+    @Test
+    fun findBTCMaxPriceTest() {
+        every { cryptocurrencyRepository.findMinMaxByName("BTC", -1) } returns cryptocurrencyBTCPrice
+        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("BTC", -1)
+        assertEquals(cryptocurrencyBTCPrice, result)
+    }
+
+    @Test
+    fun findETHMinPriceTest() {
+        every { cryptocurrencyRepository.findMinMaxByName("ETH", -1) } returns cryptocurrencyETHPrice
+        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("ETH", -1)
+        assertEquals(cryptocurrencyETHPrice, result)
+    }
+
+    @Test
+    fun findETHMaxPriceTest() {
+        every { cryptocurrencyRepository.findMinMaxByName("ETH", 1) } returns cryptocurrencyETHPrice
+        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("ETH", 1)
+        assertEquals(cryptocurrencyETHPrice, result)
+    }
+
+    @Test
+    fun findXRPMinPriceTest() {
+        every { cryptocurrencyRepository.findMinMaxByName("XRP", -1) } returns cryptocurrencyXRPPrice
+        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("XRP", -1)
+        assertEquals(cryptocurrencyXRPPrice, result)
+    }
+
+    @Test
+    fun findXRPMaxPriceTest() {
+        every { cryptocurrencyRepository.findMinMaxByName("XRP", 1) } returns cryptocurrencyXRPPrice
+        val result = cryptocurrencyService.findMinMaxPriceByCryptocurrencyName("XRP", 1)
+        assertEquals(cryptocurrencyXRPPrice, result)
+    }
+
+    @Test
+    fun getBTCPageTest() {
+        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "BTC" }
+            .sortedBy { it.price }
+        val page = PageRequest.of(0, 10)
+        val pageable = PageRequest.of(0, 10, Sort.by("price"))
+        every { cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("BTC", pageable)} returns toPage(sortedList, page)
+        val result = cryptocurrencyService.getCryptocurrencyPages("BTC", 0, 10)
+        assertEquals(sortedList, result)
+    }
+
+    @Test
+    fun getETHPageTest() {
+        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "ETH" }
+            .sortedBy { it.price }
+        val page = PageRequest.of(0, 10)
+        val pageable = PageRequest.of(0, 10, Sort.by("price"))
+        every { cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("ETH", pageable)} returns toPage(sortedList, page)
+        val result = cryptocurrencyService.getCryptocurrencyPages("ETH", 0, 10)
+        assertEquals(sortedList, result)
+    }
+
+    @Test
+    fun getXRPPageTest() {
+        val sortedList = cryptocurrencyList.filter { it.cryptocurrencyName == "XRP" }
+            .sortedBy { it.price }
+        val page = PageRequest.of(0, 10)
+        val pageable = PageRequest.of(0, 10, Sort.by("price"))
+        every { cryptocurrencyRepository.findCryptocurrencyPriceByCryptocurrencyName("XRP", pageable)} returns toPage(sortedList, page)
+        val result = cryptocurrencyService.getCryptocurrencyPages("XRP", 0, 10)
+        assertEquals(sortedList, result)
+    }
+
+    @Test
+    fun createCSV() {
+        every { cryptocurrencyRepository.findMinMaxByName("BTC", -1) } returns cryptocurrencyBTCPrice
+        every { cryptocurrencyRepository.findMinMaxByName("BTC", 1) } returns cryptocurrencyBTCPrice
+        every { cryptocurrencyRepository.findMinMaxByName("ETH", -1) } returns cryptocurrencyETHPrice
+        every { cryptocurrencyRepository.findMinMaxByName("ETH", 1) } returns cryptocurrencyETHPrice
+        every { cryptocurrencyRepository.findMinMaxByName("XRP", -1) } returns cryptocurrencyXRPPrice
+        every { cryptocurrencyRepository.findMinMaxByName("XRP", 1) } returns cryptocurrencyXRPPrice
+
+        val result = cryptocurrencyService.writeCsv("cryptocurrency-prices-test")
+        val actual = isEqual(Paths.get(expectedCsvFile), Paths.get(resultCsvFile))
+        result.delete()
+        assertEquals(true, actual)
+    }
+
+    private fun toPage(list: List<Cryptocurrency>, pageable: Pageable): Page<Cryptocurrency> {
+        val itemsInPage = list.drop(pageable.offset.toInt()).take(pageable.pageSize)
+        return PageImpl(itemsInPage, pageable, list.size.toLong())
+    }
+
+    private fun isEqual(firstFile: Path, secondFile: Path): Boolean {
+        if (Files.size(firstFile) != Files.size(secondFile)) {
+            return false
+        }
+        val first = Files.readAllBytes(firstFile)
+        val second = Files.readAllBytes(secondFile)
+        return first.contentEquals(second)
+    }
 }
